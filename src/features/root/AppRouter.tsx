a6d777459f30b9cbddaf73e93a815fd5689cd05f
vipinpaul <vipin.paul@bridgeconn.com>
2026-05-07 11:50:16 +0530

    RBAC Role Bases Access WEB

--- src/features/root/AppRouter.tsx
+++ src/features/root/AppRouter.tsx
@@ -2,12 +2,16 @@ import { RouterProvider } from '@tanstack/react-router';
 
 import { useAuth } from '@/hooks/useAuth';
 import { router } from '@/lib/router';
+import { useAppStore } from '@/store/store';
 
 export function AppRouter(): React.JSX.Element {
   const { isAuthenticated, isLoading, loginWithRedirect } = useAuth();
+  const { userdetail } = useAppStore();
   return (
     <RouterProvider
-      context={{ auth: { isAuthenticated, isLoading, loginWithRedirect } }}
+      context={{
+        auth: { isAuthenticated, isLoading, role: userdetail?.role ?? null, loginWithRedirect },
+      }}
       router={router}
     />
   );
